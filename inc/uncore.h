151aff550419f56ba70d5d7d88f993971d3a495c
sungjun <allencho1222@postech.ac.kr>
2020-12-01 00:23:11 +0900

    make ChampSim instantiate USIMM memory

--- inc/uncore.h
+++ inc/uncore.h
@@ -3,7 +3,8 @@
 
 #include "champsim.h"
 #include "cache.h"
-#include "dram_controller.h"
+//#include "dram_controller.h"
+#include "../src/usimm/memory_controller.h"
 //#include "drc_controller.h"
 
 //#define DRC_MSHR_SIZE 48
@@ -16,7 +17,7 @@ class UNCORE {
     CACHE LLC{"LLC", LLC_SET, LLC_WAY, LLC_SET*LLC_WAY, LLC_WQ_SIZE, LLC_RQ_SIZE, LLC_PQ_SIZE, LLC_MSHR_SIZE};
 
     // DRAM
-    MEMORY_CONTROLLER DRAM{"DRAM"}; 
+    MemoryController DRAM{"DRAM"}; 
 
     UNCORE(); 
 };
